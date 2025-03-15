cbedcfa0362ec62cb8f3688d6f79aa4b02e6f4ee
Vandylab <gui.vdv42@gmail.com>
2025-03-15 20:25:55 +0100

    [redshift] Add blue color filter

--- configuration.nix
+++ configuration.nix
@@ -54,6 +54,23 @@
 
   fonts.fontDir.enable = true;
 
+  services.geoclue2.enable = true;
+
+  location.provider = "geoclue2";
+
+  services.redshift = {
+    enable = true;
+    brightness = {
+      # Note the string values below.
+      day = "1";
+      night = "1";
+    };
+    temperature = {
+      day = 5500;
+      night = 3700;
+    };
+  };
+
   powerManagement = {
     enable = true;
     cpuFreqGovernor = "ondemand";
