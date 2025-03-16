f6f0f8484c3c7f5778b0a79fb5d7c71a18754a13
Vandylab <gui.vdv42@gmail.com>
2025-03-16 19:08:32 +0100

    [config] Set home-manager config as module

--- configuration.nix
+++ configuration.nix
@@ -121,98 +121,7 @@
   programs.zsh.enable = true;
   users.users.vandylab.shell = pkgs.zsh;
 
-  home-manager.users.vandylab = { pkgs, lib, ... } : {
-
-    nixpkgs.config = {
-      allowUnfree = true;
-    };
-
-    gtk = {
-      enable = true;
-      gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
-    };
-
-    home.packages = with pkgs; [
-      zsh
-      zsh-completions
-      zsh-autosuggestions
-      zsh-syntax-highlighting
-      terminator
-      docker
-      emacs
-      wget
-      git
-      zsh
-      htop
-      oh-my-zsh
-      texliveSmall
-      vscode
-      firefox
-      thunderbird
-      python313
-      python313Packages.pip
-      discord
-      slack
-    ];
-
-    programs.zsh = {
-      enable = true;
-      enableCompletion = true;
-      enableAutosuggestions = true;
-      syntaxHighlighting.enable = true;
-      shellAliases = {
-        esprint = "sudo find / \\( -name \"*~\" -o -name \"#*#\" \\) -print 2> /dev/null";
-	      esdelete = "sudo find / \\( -name \"*~\" -o -name \"#*#\" \\) -print -delete 2> /dev/null";
-      };
-      oh-my-zsh = {
-        enable = true;
-	      theme = "agnoster";
-        plugins = [
-          "git"
-          "docker"
-        ];
-      };
-    };
-
-    programs.terminator = {
-      config = {
-        profiles.default = {
-	        font = "JetBrainsMono Nerd Font Mono Semi-Bold 10";
-          use_system_font = false;
-          title_use_system_font = false;
-          title_font = "Unifont 8";
-        };
-      };
-    };
-
-    programs.vscode = {
-      enable = true;
-      extensions = with pkgs.vscode-extensions; [
-      	ms-python.debugpy
-	      ms-python.python
-	      ms-toolsai.jupyter
-	      ms-toolsai.jupyter-keymap
-	      ms-toolsai.jupyter-renderers
-	      ms-toolsai.vscode-jupyter-cell-tags
-	      ms-toolsai.vscode-jupyter-slideshow
-	      tuttieee.emacs-mcx
-      ];
-    };
-
-    home.file = {
-      ".emacs" = {
-        source = ./emacs_conf;
-        force = true;
-      };
-      "./.config/i3" = {
-        source = ./i3_config;
-        recursive = true;
-        force = true;
-      };
-    };
-
-    home.stateVersion = "24.11";
-  };
+  home-manager.users.vandylab = import ./home-manager-config.nix;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -227,8 +136,8 @@
     picom
     networkmanagerapplet
     pulseaudio pavucontrol
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
   ];
 
   hardware.acpilight.enable = true;
---
+++ home-manager-config.nix
@@ -0,0 +1,92 @@
+{ pkgs, lib, ... } : {
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  gtk = {
+    enable = true;
+    gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+  };
+
+  home.packages = with pkgs; [
+    zsh
+    zsh-completions
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+    terminator
+    docker
+    emacs
+    wget
+    git
+    zsh
+    htop
+    oh-my-zsh
+    texliveSmall
+    vscode
+    firefox
+    thunderbird
+    python313
+    python313Packages.pip
+    discord
+    slack
+  ];
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      esprint = "sudo find / \\( -name \"*~\" -o -name \"#*#\" \\) -print 2> /dev/null";
+	    esdelete = "sudo find / \\( -name \"*~\" -o -name \"#*#\" \\) -print -delete 2> /dev/null";
+    };
+    oh-my-zsh = {
+      enable = true;
+	    theme = "agnoster";
+      plugins = [
+        "git"
+        "docker"
+      ];
+    };
+  };
+
+  programs.terminator = {
+    config = {
+      profiles.default = {
+	      font = "JetBrainsMono Nerd Font Mono Semi-Bold 10";
+        use_system_font = false;
+        title_use_system_font = false;
+        title_font = "Unifont 8";
+      };
+    };
+  };
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      ms-python.debugpy
+	    ms-python.python
+	    ms-toolsai.jupyter
+	    ms-toolsai.jupyter-keymap
+	    ms-toolsai.jupyter-renderers
+	    ms-toolsai.vscode-jupyter-cell-tags
+	    ms-toolsai.vscode-jupyter-slideshow
+	    tuttieee.emacs-mcx
+    ];
+  };
+
+  home.file = {
+    ".emacs" = {
+      source = ./emacs_conf;
+      force = true;
+    };
+    "./.config/i3" = {
+      source = ./i3_config;
+      recursive = true;
+      force = true;
+    };
+  };
+
+  home.stateVersion = "24.11";
+}
